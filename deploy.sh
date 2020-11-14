bed39c621448684ccb31204d29e2c0b70b5eb920
xieshan <1140584614@qq.com>
2020-11-14 14:19:49 +0800

    chmod modify

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 ps -ef | grep questionnaire-api| grep -v grep | awk '{print $2}' | xargs kill -9
 cd ~/go/src/questionnaire/qustionnire-api/
-git pull https://github.com/KyrieIrving2/questionnire-api.git
+git pull
 ./questionnaire-api &
