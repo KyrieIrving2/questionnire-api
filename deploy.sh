37118c7cc783e4149ef9405930a52d1e2e501dd5
v_shangxie(谢善) <v_shangxie@tencent.com>
2020-11-14 13:25:11 +0800

    shell modify

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 ! /bin/sh
 
 ps -ef | grep questionnaire-api| grep -v grep | awk '{print $2}' | xargs kill -9
-cd ~/go/src/questionnaire/
+cd ~/go/src/questionnaire/qustionnire-api/
 git pull https://github.com/KyrieIrving2/questionnire-api.git
 ./questionnaire-api &
